fd16527225fad9a4b96db809049ee812f7f07596
Not <simond.hugo@gmail.com>
2024-10-29 12:55:53 +0100

    [ADD] = "Tailwind"; "LoaderView"

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Bonsoir on test la modification</h1>
   </div>
 </template>
 
---
+++ src/views/LoaderView.vue
@@ -0,0 +1,22 @@
+<template>
+
+  <div class="mx-auto w-[500px] bg-gray-950 rounded-xl overflow-hidden drop-shadow-xl">
+    <div class="bg-[rgb(41,41,41)] flex items-center p-[5px] text-whitec relative">
+      <div class="flex absolute left-3">
+        <span class="h-3.5 w-3.5 bg-[#f32d2a] rounded-xl mr-2"></span>
+        <span class="h-3.5 w-3.5 bg-[#f5e72b] rounded-xl mr-2"></span>
+        <span class="h-3.5 w-3.5 bg-[#00b144] rounded-xl"></span>
+      </div>
+      <div class="flex-1 text-center text-white">Error 404 Not Found</div>
+    </div>
+    <div class="p-2.5 text-[#c1c2c1]">
+      <div>
+        <span class="mr-2">Loading</span>
+        <span class="animate-[ping_1.5s_0.5s_ease-in-out_infinite]">.</span>
+        <span class="animate-[ping_1.5s_0.7s_ease-in-out_infinite]">.</span>
+        <span class="animate-[ping_1.5s_0.9s_ease-in-out_infinite]">.</span>
+      </div>
+    </div>
+  </div>
+
+</template>
